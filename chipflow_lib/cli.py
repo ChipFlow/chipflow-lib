868d9c62da26a97312e7119d44585e1e47bbd422
Catherine Zotova <catherine.zotova@chipflow.io>
2023-05-02 17:19:23 +0000

    cli: reorganize in topological order to make easier to read. NFCI

--- chipflow_lib/cli.py
+++ chipflow_lib/cli.py
@@ -13,51 +13,13 @@ class ChipFlowError(Exception):
 
 
 class Main():
-    def run_sim(self, args):
-        if args.sim_action == "build-yosys":
-            return self._sim_build_yosys()
-
-        module_loc = self.config["chipflow"]["sim_module"]
-        doit_build_module = self._load_module(module_loc + ".doit_build")
-
-        cmd = ["build_sim"]
-        DoitMain(ModuleTaskLoader(doit_build_module)).run(cmd)
-
-    def run_software(self, args):
-        module_loc = self.config["chipflow"]["software_module"]
-        doit_build_module = self._load_module(module_loc + ".doit_build")
-
-        cmd = ["build_software"]
-        DoitMain(ModuleTaskLoader(doit_build_module)).run(cmd)
-
-    def run_board(self, args):
-        context = self._load("load_board_context")
-        context.build()
-
-    def run_silicon_rtlil(self, args):
-        context = self._load("load_silicon_context")
-        context.build()
-
-    def run_path(self, args):
-        print(os.path.dirname(__file__))
-
-    def run(self):
-        parser = self._build_arg_parser()
-
-        args = parser.parse_args(sys.argv[1:])
-
-        self._parse_config(args)
-
-        getattr(self, 'run_' + args.action)(args)
-
-    def _sim_build_yosys(self):
-        context = self._load("load_sim_context")
-        context.build()
-
     def _build_arg_parser(self):
         parser = argparse.ArgumentParser()
 
         parser_action = parser.add_subparsers(dest="action", required=True)
+
+        # Utility
+        path_parser = parser_action.add_parser("path", help="Get path of module.")
 
         # Sim
         sim_parser = parser_action.add_parser("sim", help="Simulate the design.")
@@ -66,16 +28,15 @@ class Main():
         sim_action.add_parser("build-yosys", help="Build the intermediate yosys simulation.")
 
         # Board
-        parser_action.add_parser("board", help="Build the design for a board.")
-        parser_action.add_parser("path", help="Get path of module.")
+        board_parser = parser_action.add_parser("board", help="Build the design for a board.")
+
+        # Silicon
+        silicon_rtlil_parser = parser_action.add_parser("silicon_rtlil", help="Generate RTLIL for silicon.")
 
         # Software/BIOS
         software_parser = parser_action.add_parser("software", help="Software.")
         software_action = software_parser.add_subparsers(dest="software_action")
         software_action.add_parser("build", help="Build.")
-
-        # Silicon
-        parser_action.add_parser("silicon_rtlil", help="Generate RTLIL for silicon.")
 
         return parser
 
@@ -86,6 +47,15 @@ class Main():
         # TODO: Add better validation/errors for loading chipflow.toml
         with open(config_file, mode="rb") as fp:
             self.config = tomli.load(fp)
+
+    def run(self):
+        parser = self._build_arg_parser()
+
+        args = parser.parse_args(sys.argv[1:])
+
+        self._parse_config(args)
+
+        getattr(self, 'run_' + args.action)(args)
 
     def _load(self, loader_name):
         try:
@@ -100,9 +70,6 @@ class Main():
 
         return getattr(module, loader_name)(self.config)
 
-    def _load_design_module(self):
-        return self._load_module(self.config["chipflow"]["design_module"])
-
     def _load_module(self, module_loc):
         try:
             module = importlib.import_module(module_loc)
@@ -110,6 +77,41 @@ class Main():
             raise ChipFlowError("Could not load module, {module_loc}.") from error
 
         return module
+
+    def _load_design_module(self):
+        return self._load_module(self.config["chipflow"]["design_module"])
+
+    def run_path(self, args):
+        print(os.path.dirname(__file__))
+
+    def _sim_build_yosys(self):
+        context = self._load("load_sim_context")
+        context.build()
+
+    def run_sim(self, args):
+        if args.sim_action == "build-yosys":
+            return self._sim_build_yosys()
+
+        module_loc = self.config["chipflow"]["sim_module"]
+        doit_build_module = self._load_module(module_loc + ".doit_build")
+
+        cmd = ["build_sim"]
+        DoitMain(ModuleTaskLoader(doit_build_module)).run(cmd)
+
+    def run_board(self, args):
+        context = self._load("load_board_context")
+        context.build()
+
+    def run_silicon_rtlil(self, args):
+        context = self._load("load_silicon_context")
+        context.build()
+
+    def run_software(self, args):
+        module_loc = self.config["chipflow"]["software_module"]
+        doit_build_module = self._load_module(module_loc + ".doit_build")
+
+        cmd = ["build_software"]
+        DoitMain(ModuleTaskLoader(doit_build_module)).run(cmd)
 
 
 if __name__ == '__main__':
