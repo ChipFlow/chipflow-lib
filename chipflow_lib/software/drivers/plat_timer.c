29c3e1ee9c9871e1e3b77937800bb524cb0c7114
Jean-François Nguyen <jf@jfng.fr>
2024-02-15 15:41:39 +0100

    software: update plat_timer driver to latest amaranth-orchard changes.

--- chipflow_lib/software/drivers/plat_timer.c
+++ chipflow_lib/software/drivers/plat_timer.c
@@ -2,16 +2,13 @@
 #include "plat_timer.h"
 
 uint64_t plat_timer_read(volatile plat_timer_regs_t *timer) {
-	uint32_t low_val;
-	uint32_t high_val;
-	do {
-		high_val = timer->high;
-		low_val = timer->low;
-	} while (high_val != timer->high); // wrapped around
-	return (((uint64_t)high_val) << 32ULL) | low_val;
+	uint32_t cnt_lo = timer->cnt_lo;
+	__asm__ volatile ("" : : : "memory");
+	return (((uint64_t)timer->cnt_hi) << 32ULL) | cnt_lo;
 }
 
 void plat_timer_schedule(volatile plat_timer_regs_t *timer, uint64_t val) {
-	timer->high = (val >> 32U) & 0xFFFFFFFFU;
-	timer->low = val & 0xFFFFFFFFU;
+	timer->cmp_lo = val & 0xFFFFFFFFU;
+	__asm__ volatile ("" : : : "memory");
+	timer->cmp_hi = (val >> 32U) & 0xFFFFFFFFU;
 }
--- chipflow_lib/software/drivers/plat_timer.h
+++ chipflow_lib/software/drivers/plat_timer.h
@@ -5,8 +5,10 @@
 #include <stdint.h>
 
 typedef struct __attribute__((packed)) {
-	uint32_t low;
-	uint32_t high;
+	uint32_t cnt_lo;
+	uint32_t cnt_hi;
+	uint32_t cmp_lo;
+	uint32_t cmp_hi;
 } plat_timer_regs_t;
 
 uint64_t plat_timer_read(volatile plat_timer_regs_t *timer);
