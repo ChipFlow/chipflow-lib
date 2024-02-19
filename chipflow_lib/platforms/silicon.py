b2ec4753f4076764a3b83e78212e5f73fa14e2fe
Jean-François Nguyen <jf@jfng.fr>
2024-02-19 16:39:49 +0100

    platforms.silicon: add domain argument to Fragment.add_statements().
    This is required since amaranth-lang/amaranth@6e06fc013f54.

--- chipflow_lib/platforms/silicon.py
+++ chipflow_lib/platforms/silicon.py
@@ -71,17 +71,17 @@ class SiliconPlatform:
 
             if pad["type"] in ("io", "i", "clk"):
                 port_i = Signal(name=f"io${pad_name}$i")
-                fragment.add_statements(pin.i.eq(port_i))
+                fragment.add_statements("comb", pin.i.eq(port_i))
                 fragment.add_driver(pin.i)
                 ports.append(port_i)
             if pad["type"] in ("oe", "io", "o"):
                 port_o = Signal(name=f"io${pad_name}$o")
-                fragment.add_statements(port_o.eq(pin.o))
+                fragment.add_statements("comb", port_o.eq(pin.o))
                 fragment.add_driver(port_o)
                 ports.append(port_o)
             if pad["type"] in ("oe", "io"):
                 port_oe = Signal(name=f"io${pad_name}$oe")
-                fragment.add_statements(port_oe.eq(pin.oe))
+                fragment.add_statements("comb", port_oe.eq(pin.oe))
                 fragment.add_driver(port_oe)
                 ports.append(port_oe)
 
