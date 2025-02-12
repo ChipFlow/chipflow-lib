ef0bb641872b592b5e9abb2b213d9c8fca048743
Rob Taylor <rob.taylor@chipflow.io>
2025-02-12 22:51:02 +0000

    feat: Enable configuring of package pin allocation from the package definition
    Also enables grid array and quad format packages

--- chipflow_lib/platforms/__init__.py
+++ chipflow_lib/platforms/__init__.py
@@ -10,6 +10,11 @@ from .silicon import *
 from .sim import *
 from .utils import *
 
-__all__ = ['PIN_ANNOTATION_SCHEMA', 'IOSignature',
+__all__ = ['IO_ANNOTATION_SCHEMA', 'IOSignature', 'IOModel',
            'OutputIOSignature', 'InputIOSignature', 'BidirIOSignature',
-           'load_pinlock', "PACKAGE_DEFINITIONS", 'top_interfaces']
+           'load_pinlock', "PACKAGE_DEFINITIONS", 'top_components', 'LockFile',
+           'Package', 'PortMap', 'Port', 'Process',
+           'GAPackageDef', 'QuadPackageDef', 'BareDiePackageDef', 'BasePackageDef',
+           'BringupPins', 'JTAGPins', 'PowerPins',
+           'SiliconPlatformPort', 'SiliconPlatform',
+           'SimPlatform']
