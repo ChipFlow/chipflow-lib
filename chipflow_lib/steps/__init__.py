cb83ab87e5af145921ab8f8268e716c061667640
Rob Taylor <rob.taylor@chipflow.io>
2025-05-20 14:05:42 +0100

    Setup amaranth tools to use our yowasp build

--- chipflow_lib/steps/__init__.py
+++ chipflow_lib/steps/__init__.py
@@ -2,7 +2,28 @@
 Steps provide an extensible way to modify the `chipflow` command behavior for a given design
 """
 
+import os
 from abc import ABC
+
+def setup_amaranth_tools():
+    _amaranth_settings = {
+        "AMARANTH_USE_YOSYS": "system",
+        "YOSYS": "yowasp-yosys",
+        "SBY": "yowasp-sby",
+        "SMTBMC": "yowasp-yosys-smtbmc",
+        "NEXTPNR_ICE40": "yowasp-nextpnr-ice40",
+        "ICEPACK": "yowasp-icepackr",
+        "NEXTPNR_ECP5": "yowasp-nextpnr-ecp5",
+        "ECPBRAM": "yowasp-ecpbram",
+        "ECPMULTI": "yowasp-ecpmulti",
+        "ECPPACK": "yowasp-ecppack",
+        "ECPPLL": "yowasp-ecppll",
+        "ECPUNPACK": "yowasp-ecpunpack",
+        "NEXTPNR-ECP5": "yowasp-nextpnr-ecp5",
+        "YOSYS-WITNESS": "yowasp-yosys-witness",
+    }
+
+    os.environ |= _amaranth_settings
 
 class StepBase(ABC):
     def __init__(self, config={}):
