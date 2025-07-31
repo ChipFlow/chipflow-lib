be7f4e7cc0f89fc9a8ba2c82c348b62cecf72dfb
Rob Taylor <rob.taylor@chipflow.io>
2025-07-31 18:09:25 +0100

    Make Pin a TypeVar and asssign power pins for OpenFrame

--- chipflow_lib/platforms/_packages.py
+++ chipflow_lib/platforms/_packages.py
@@ -1,10 +1,11 @@
-from ._utils import QuadPackageDef, BareDiePackageDef, Package
+from ._utils import QuadPackageDef, BareDiePackageDef, GAPackageDef, Package
 from ._openframe import OpenframePackageDef
 
 # Add any new package types to both PACKAGE_DEFINITIONS and the PackageDef union
 PACKAGE_DEFINITIONS = {
     "pga144": QuadPackageDef(name="pga144", width=36, height=36),
     "cf20": BareDiePackageDef(name="cf20", width=7, height=3),
+    "bga144": GAPackageDef(name="bga144", width=36, height=36),
     "openframe": OpenframePackageDef()
 }
 
