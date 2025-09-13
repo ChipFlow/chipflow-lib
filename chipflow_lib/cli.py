c7b4258dd83e763fbd143e3907ff8c8abe571419
Rob Taylor <rob.taylor@chipflow.io>
2025-09-13 16:02:58 +0200

    Fix issue when there is no steps section in chipflow.toml

--- chipflow_lib/cli.py
+++ chipflow_lib/cli.py
@@ -37,13 +37,16 @@ def run(argv=sys.argv[1:]):
 
     if config.chipflow.steps:
         steps = DEFAULT_STEPS |config.chipflow.steps
-        for step_name, step_reference in steps.items():
-            step_cls = _get_cls_by_reference(step_reference, context=f"step `{step_name}`")
-            try:
-                commands[step_name] = step_cls(config)
-            except Exception:
-                raise ChipFlowError(f"Encountered error while initializing step `{step_name}` "
-                                f"using `{step_reference}`")
+    else:
+        steps = DEFAULT_STEPS
+
+    for step_name, step_reference in steps.items():
+        step_cls = _get_cls_by_reference(step_reference, context=f"step `{step_name}`")
+        try:
+            commands[step_name] = step_cls(config)
+        except Exception:
+            raise ChipFlowError(f"Encountered error while initializing step `{step_name}` "
+                            f"using `{step_reference}`")
 
     parser = argparse.ArgumentParser(
         prog="chipflow",
